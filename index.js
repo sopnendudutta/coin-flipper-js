f361b5590620c505bddd45f1b16cf8a46120fa15
sopnendudutta <sopnendu7@gmail.com>
2026-04-22 17:21:10 +0530

    Initial commit:Coin flip project finished

---
+++ index.js
@@ -0,0 +1,44 @@
+let heads = 0;
+let tails = 0;
+let coin = document.querySelector(".coin");
+let flipBtn = document.querySelector("#flip-button");
+let resetBtn = document.querySelector("#reset-button");
+
+flipBtn.addEventListener("click", () => {
+    let i = Math.floor(Math.random() * 2);
+    coin.style.animation = "none";
+    if (i) {
+        setTimeout(function () {
+            coin.style.animation = "spin-heads 3s forwards";
+        }, 100);
+        heads++;
+    } else {
+        setTimeout(function () {
+            coin.style.animation = "spin-tails 3s forwards";
+        }, 100);
+        tails++;
+    }
+    setTimeout(updateStats, 3000);
+    disableButton();
+})
+
+function updateStats() {
+    document.querySelector("#heads-count").textContent = `heads: ${heads}`;
+    document.querySelector("#tails-count").textContent = `tails: ${tails}`;
+}
+
+function disableButton() {
+    flipBtn.disabled = true;
+    setTimeout(() => {
+        flipBtn.disabled = false;
+    }, 3000);
+}
+
+
+resetBtn.addEventListener("click", () => {
+    coin.style.animation = "none";
+    coin.style.transform = "rotateY(0deg)";
+    heads = 0;
+    tails = 0;
+    updateStats();
+})
